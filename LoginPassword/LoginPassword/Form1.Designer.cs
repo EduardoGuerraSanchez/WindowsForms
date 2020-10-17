ee89ab5d8924612bbe71e51fc23adc18d84b7812
EduardoGuerraSanchez <eduardoguerra015@gmail.com>
2020-10-17 21:41:52 +0200

    Ejercicio 3

--- LoginPassword/LoginPassword/Form1.Designer.cs
+++ LoginPassword/LoginPassword/Form1.Designer.cs
@@ -33,12 +33,17 @@
             this.textoLogin = new System.Windows.Forms.TextBox();
             this.textoPassword = new System.Windows.Forms.TextBox();
             this.iniciar = new System.Windows.Forms.Button();
+            this.textoBienvenido = new System.Windows.Forms.TextBox();
+            this.limpiar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(246, 180);
+            this.label1.Location = new System.Drawing.Point(38, 158);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(33, 13);
             this.label1.TabIndex = 0;
@@ -47,7 +52,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(246, 240);
+            this.label2.Location = new System.Drawing.Point(38, 223);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(53, 13);
             this.label2.TabIndex = 1;
@@ -56,7 +61,7 @@
             // 
             // textoLogin
             // 
-            this.textoLogin.Location = new System.Drawing.Point(336, 173);
+            this.textoLogin.Location = new System.Drawing.Point(128, 151);
             this.textoLogin.Name = "textoLogin";
             this.textoLogin.Size = new System.Drawing.Size(100, 20);
             this.textoLogin.TabIndex = 2;
@@ -64,25 +69,76 @@
             // 
             // textoPassword
             // 
-            this.textoPassword.Location = new System.Drawing.Point(336, 233);
+            this.textoPassword.Location = new System.Drawing.Point(128, 216);
             this.textoPassword.Name = "textoPassword";
             this.textoPassword.Size = new System.Drawing.Size(100, 20);
             this.textoPassword.TabIndex = 3;
             // 
             // iniciar
             // 
-            this.iniciar.Location = new System.Drawing.Point(350, 285);
+            this.iniciar.Location = new System.Drawing.Point(153, 274);
             this.iniciar.Name = "iniciar";
             this.iniciar.Size = new System.Drawing.Size(75, 23);
             this.iniciar.TabIndex = 4;
             this.iniciar.Text = "Iniciar";
             this.iniciar.UseVisualStyleBackColor = true;
+            this.iniciar.Click += new System.EventHandler(this.iniciar_Click);
+            // 
+            // textoBienvenido
+            // 
+            this.textoBienvenido.Location = new System.Drawing.Point(308, 188);
+            this.textoBienvenido.Name = "textoBienvenido";
+            this.textoBienvenido.Size = new System.Drawing.Size(173, 20);
+            this.textoBienvenido.TabIndex = 5;
+            // 
+            // limpiar
+            // 
+            this.limpiar.Location = new System.Drawing.Point(41, 274);
+            this.limpiar.Name = "limpiar";
+            this.limpiar.Size = new System.Drawing.Size(75, 23);
+            this.limpiar.TabIndex = 6;
+            this.limpiar.Text = "Limpiar";
+            this.limpiar.UseVisualStyleBackColor = true;
+            this.limpiar.Click += new System.EventHandler(this.limpiar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(231, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Pista: usuario: Mi nombre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(222, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(148, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Pista: contraseña: contraseña";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(305, 154);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(218, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Aqui te mostraremos si has iniciado bien o no";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(573, 360);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.limpiar);
+            this.Controls.Add(this.textoBienvenido);
             this.Controls.Add(this.iniciar);
             this.Controls.Add(this.textoPassword);
             this.Controls.Add(this.textoLogin);
@@ -102,6 +158,11 @@
         private System.Windows.Forms.TextBox textoLogin;
         private System.Windows.Forms.TextBox textoPassword;
         private System.Windows.Forms.Button iniciar;
+        private System.Windows.Forms.TextBox textoBienvenido;
+        private System.Windows.Forms.Button limpiar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- LoginPassword/LoginPassword/Form1.cs
+++ LoginPassword/LoginPassword/Form1.cs
@@ -24,11 +24,46 @@ namespace LoginPassword
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
+            
+        }
+
+        private void iniciar_Click(object sender, EventArgs e)
+        {
+            bool log = false;
+            bool pass = false;
             String login = "edu";
-            if(textoLogin.Text == login)
+            String password = "contraseña";
+            if (textoLogin.Text == login)
             {
-                Console.WriteLine("Perfecto");
+                log = true;
             }
+
+            if(textoPassword.Text == password)
+            {
+                pass = true;
+            }
+
+            if( log == true && pass == true)
+            {
+                textoBienvenido.Text = "Bienvenido";
+            }
+            else
+            {
+                textoBienvenido.Text = "Error, vuelve a introducir los datos";
+            }
+
+        }
+
+        private void limpiar_Click(object sender, EventArgs e)
+        {
+            textoLogin.Text = "";
+            textoPassword.Text = "";
+            textoBienvenido.Text = "";
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
