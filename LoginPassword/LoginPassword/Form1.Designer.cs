6f7b344d8756801e93c9ba0eacc5bbdd684eed0b
EduardoGuerraSanchez <eduardoguerra015@gmail.com>
2020-10-17 20:42:06 +0200

    Ejercicio 3

---
+++ LoginPassword/LoginPassword/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace LoginPassword
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textoLogin = new System.Windows.Forms.TextBox();
+            this.textoPassword = new System.Windows.Forms.TextBox();
+            this.iniciar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(246, 180);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(246, 240);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textoLogin
+            // 
+            this.textoLogin.Location = new System.Drawing.Point(336, 173);
+            this.textoLogin.Name = "textoLogin";
+            this.textoLogin.Size = new System.Drawing.Size(100, 20);
+            this.textoLogin.TabIndex = 2;
+            this.textoLogin.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textoPassword
+            // 
+            this.textoPassword.Location = new System.Drawing.Point(336, 233);
+            this.textoPassword.Name = "textoPassword";
+            this.textoPassword.Size = new System.Drawing.Size(100, 20);
+            this.textoPassword.TabIndex = 3;
+            // 
+            // iniciar
+            // 
+            this.iniciar.Location = new System.Drawing.Point(350, 285);
+            this.iniciar.Name = "iniciar";
+            this.iniciar.Size = new System.Drawing.Size(75, 23);
+            this.iniciar.TabIndex = 4;
+            this.iniciar.Text = "Iniciar";
+            this.iniciar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.iniciar);
+            this.Controls.Add(this.textoPassword);
+            this.Controls.Add(this.textoLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Registro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textoLogin;
+        private System.Windows.Forms.TextBox textoPassword;
+        private System.Windows.Forms.Button iniciar;
+    }
+}
+
